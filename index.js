35ef341980a17a6fa757a144b6c4fcdc52bbecdc
Nasake <wilfrid.salles-otta@supinternet.fr>
2025-02-14 11:59:59 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import { createClient } from '@supabase/supabase-js'
+import 'dotenv/config'
+const { SUPABASE_DB_API_KEY : key, SUPABASE_DB_URL : url } = process.env
+export const supabase = createClient(url, key);
+
+async function getPlacesByType(type) {
+    try {
+        const { data, error } = await supabase
+            .from('places')
+            .select("id, type")
+            .eq('type', type)
+        if(error) throw error;
+        return data;
+    } catch (error) {
+        //console.log(error);
+        return [];
+    }
+}
+
+function getPlacesByTypeTest() {
+    
+}
+
+async function addCar() {
+    try {
+        const { data, error } = await supabase
+            .from('voitures')
+            .insert({ plaque: 'Metalosse', type: 'citadine' })
+            .select()
+            if(error) throw error;
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// addCar();
+console.log(await getPlacesByType('citadine'))
