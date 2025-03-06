81c8eaf70c963185625918a65a73adb226b0414e
Nasake <sallesottawilfrid@gmail.com>
2025-03-06 15:41:30 +0100

    Express routes

--- index.js
+++ index.js
@@ -1,6 +1,92 @@
-const checkOccupiedPlace = require("./functions/checkOccupiedPlace");
-const checkParkedCar = require("./functions/checkParkedCar");
-const countFreePlaces = require("./functions/countFreePlaces");
+const checkOccupiedPlace = require("./functions/checkOccupiedPlace")
+const checkParkedCar = require("./functions/checkParkedCar")
+const countFreePlaces = require("./functions/countFreePlaces")
+const supabase = require('./utils/supabase')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/parking', (req, res) => {
+    res.send('parking')
+})
+
+app.get('/reservations', async (req, res) => {
+    const { data, error } = await supabase
+    .from('reservations')
+    .select()
+    res.send(data);
+})
+
+app.get('/reservations/:id', async (req, res) => {
+    const { id } = req.params; // Récupère l'ID depuis l'URL
+
+    const { data, error } = await supabase
+        .from('reservations')
+        .select()
+        .eq('id', id) // Filtre par ID
+        .single(); // Assure qu'on récupère un seul élément
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+
+    res.json(data);
+})
+
+app.post('/reservations/new', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/place', async (req, res) => {
+    const { data, error } = await supabase
+    .from('places')
+    .select()
+    res.send(data);
+})
+
+app.get('/place/:id', async (req, res) => { 
+    const { id } = req.params; // Récupère l'ID depuis l'URL
+
+    const { data, error } = await supabase
+        .from('places')
+        .select()
+        .eq('id', id) // Filtre par ID
+        .single(); // Assure qu'on récupère un seul élément
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+
+    res.json(data);
+});
+
+app.get('/place/type/:type', async (req, res) => { 
+    const { type } = req.params; // Récupère le type depuis l'URL
+
+    const { data, error } = await supabase
+        .from('places')
+        .select()
+        .eq('type', type) // Filtre par type
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+
+    res.json(data);
+});
+
+app.get('/scan', (req, res) => {
+    res.send('Page scan')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
 
 //countFreePlaces().then(console.log);
 
